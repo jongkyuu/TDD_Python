32899218e553ec95e17e39f95869038e7b267783
minerva421 <minerva421.ai@gmail.com>
2022-10-27 13:25:03 +0900

    TDD - 20장

--- tdd.py
+++ tdd.py
@@ -9,6 +9,7 @@ class TestCase:
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
         
 class WasRun(TestCase):
     def __init__(self, name):
@@ -16,26 +17,34 @@ class WasRun(TestCase):
         super().__init__(name)
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
     
     def setUp(self):
-        self.wasRun = 1
-        self.wasSetUp = 1
+        self.log = "setUp "
+        
+    def tearDown(self):
+        self.log += "tearDown "
+
         
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
         
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
+    # def testRunning(self):
+    #     self.test.run()
+    #     assert(self.test.wasRun)
         
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
+    
+    def tearDown(self):
+        pass
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+# TestCaseTest("testRunning").run()
+# TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
 
 # test = WasRun("testMethod")
 # print(test.wasRun)
