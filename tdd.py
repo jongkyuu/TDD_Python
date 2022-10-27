151cfe062edf414b27e7a1f2d588d5c33a1ee046
minerva421 <minerva421.ai@gmail.com>
2022-10-27 13:03:42 +0900

    TDD - 19장 Test 단순화 후

--- tdd.py
+++ tdd.py
@@ -23,18 +23,16 @@ class WasRun(TestCase):
         self.wasSetUp = 1
         
 class TestCaseTest(TestCase):
-    def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+    def setUp(self):
+        self.test = WasRun("testMethod")
         
-    def testSetUp(sefl):
-        test = WasRun("testMethod")
-        test.run()
-        print(test.wasSetUp)
-        assert(test.wasSetUp)
-    
+    def testRunning(self):
+        self.test.run()
+        assert(self.test.wasRun)
+        
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
 TestCaseTest("testSetUp").run()
