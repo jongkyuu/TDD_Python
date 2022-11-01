acb23ccd32b99180cd1e8a64b403cceb5732ebea
minerva421 <minerva421.ai@gmail.com>
2022-11-02 02:55:31 +0900

    코드 정리

--- tdd.py
+++ tdd.py
@@ -16,11 +16,11 @@ class WasRun(TestCase):
         self.wasRun = None
         super().__init__(name)
 
-    def testMethod(self):
-        self.log = self.log + "testMethod "
-    
     def setUp(self):
         self.log = "setUp "
+
+    def testMethod(self):
+        self.log = self.log + "testMethod "
         
     def tearDown(self):
         self.log += "tearDown "
@@ -30,10 +30,6 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
         
-    # def testRunning(self):
-    #     self.test.run()
-    #     assert(self.test.wasRun)
-        
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
@@ -42,11 +38,4 @@ class TestCaseTest(TestCase):
     def tearDown(self):
         pass
 
-# TestCaseTest("testRunning").run()
-# TestCaseTest("testSetUp").run()
 TestCaseTest("testTemplateMethod").run()
-
-# test = WasRun("testMethod")
-# print(test.wasRun)
-# test.run()
-# print(test.wasRun)
